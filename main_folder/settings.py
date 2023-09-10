468b297aad98b289df237d18f2288f73829aeb37
Omar Hosny <bettercallguts@gmail.com>
2023-09-10 08:04:28 +0300

    added the batterio them kinda -_-

--- main_folder/settings.py
+++ main_folder/settings.py
@@ -71,7 +71,7 @@ USE_I18N = True;
 USE_TZ = True;
 STATIC_URL = 'static/';
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main_folder/static')]
-STATIC_ROOT      = os.path.join(BASE_DIR, 'staticroot')
+STATIC_ROOT      = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL  = 'media/';
 
